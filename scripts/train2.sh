0431baa71c91782c2573d98c6c1bf98c61f51548
o00O00o <gaoyibo157@gmail.com>
2021-02-19 10:48:21 +0800

    edit scripts

--- scripts/train2.sh
+++ scripts/train2.sh
@@ -1,5 +1,6 @@
 cd ..
-srun -p MIA -n1 --gres=gpu:1 --mpi=pmi2 --job-name=arterney-seg --kill-on-bad-exit=1 -w SH-IDC1-10-5-30-228\ 
-python -u coarse.py\ 
---experiment_name coarse-all_label\ 
+srun -p MIA -n1 --gres=gpu:1 --mpi=pmi2 --job-name=arterney-seg --kill-on-bad-exit=1 \
+-w SH-IDC1-10-5-30-228 \
+python -u coarse.py \
+--experiment_name coarse-all_label \
 --all_label
